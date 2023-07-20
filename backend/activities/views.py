bd77e07d429730985b6527d2fdca842ee9968ecb
eeeunbiiii <121873609+eeeunbiiii@users.noreply.github.com>
2023-07-20 20:08:47 +0900

    update views (serializer)

--- backend/activities/views.py
+++ backend/activities/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializer import NewsSerializer
+from .models import News
 
-# Create your views here.
+class NewsWiewSet(viewsets.ModelViewSet):
+    queryset = News.objects.all()
+    serializer_class = NewsSerializer
