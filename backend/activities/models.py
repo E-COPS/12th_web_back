103acf889639f9c28e20e3293589542bb68f0bc3
eeeunbiiii <121873609+eeeunbiiii@users.noreply.github.com>
2023-07-20 20:08:18 +0900

    add serializer

--- backend/activities/models.py
+++ backend/activities/models.py
@@ -5,7 +5,3 @@ class News(models.Model):
     year = models.TextField(default='')
     link = models.TextField(default = 'http://127.0.0.1:8000/')
     img = models.TextField(default = '')
-    
-    def __str__(self):
-        return f'News({self.title}, {self.author}, {self.year}, {self.link}, {self.img})'
-# Create your models here.
