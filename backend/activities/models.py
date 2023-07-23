7932bf7633d524054771b571e5672410c6c43688
eeeunbiiii <121873609+eeeunbiiii@users.noreply.github.com>
2023-07-23 19:25:11 +0900

    year 수정

--- backend/activities/models.py
+++ backend/activities/models.py
@@ -2,6 +2,6 @@ from django.db import models
 class News(models.Model):
     title = models.CharField(max_length=70)
     author = models.TextField(default='')
-    year = models.TextField(default='')
+    year = models.IntegerField(max_length=3)
     link = models.TextField(default = 'http://127.0.0.1:8000/')
     img = models.TextField(default = '')
