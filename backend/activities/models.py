556379c18a2634dcbceca70801657a432dff19b7
eeeunbiiii <121873609+eeeunbiiii@users.noreply.github.com>
2023-07-25 18:38:43 +0900

    update model

--- backend/activities/models.py
+++ backend/activities/models.py
@@ -2,6 +2,8 @@ from django.db import models
 class News(models.Model):
     title = models.CharField(max_length=70)
     author = models.TextField(default='')
-    year = models.IntegerField(max_length=3)
+    year = models.IntegerField()
     link = models.TextField(default = 'http://127.0.0.1:8000/')
     img = models.TextField(default = '')
+    activity_descrption = models.TextField(default='')
+    type = models.TextField(default='')
