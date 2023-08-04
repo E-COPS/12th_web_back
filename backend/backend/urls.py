8922024a75c02ddcf3cb67d64e94adcf5941a4f7
박지은 <31093793+lucykorea414@users.noreply.github.com>
2023-08-04 20:42:34 +0900

    fix: module add

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,7 +19,8 @@ from django.urls import path, include, re_path
 from rest_framework.permissions import AllowAny
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 schema_url_patterns = [
