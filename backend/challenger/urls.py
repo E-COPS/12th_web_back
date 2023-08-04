2654beb06b2ef4c58777a1ac2359e0563b7bcc73
박지은 <31093793+lucykorea414@users.noreply.github.com>
2023-08-04 22:23:45 +0900

    text: challenger 중복된 url명 제거

--- backend/challenger/urls.py
+++ backend/challenger/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import ChallengerViewSet
 
 router = DefaultRouter()
-router.register('challenger', ChallengerViewSet)
+router.register('', ChallengerViewSet)
 
 urlpatterns = [
    path('', include(router.urls))
