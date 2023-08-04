a6525b76bb01c2a936581fd71c9db5fda0243e58
박지은 <31093793+lucykorea414@users.noreply.github.com>
2023-08-04 22:33:51 +0900

    fix: image upload path edit

--- backend/beginner/models.py
+++ backend/beginner/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class Beginner(models.Model):
     year = models.IntegerField(max_length=3, null=True, blank=True)
     track = models.CharField(max_length=100, null=True, blank=True)
-    img = models.ImageField(upload_to='static', null=True, blank=True)
+    img = models.ImageField(upload_to='static/beginner', null=True, blank=True)
     program_name = models.CharField(max_length=100, null=True, blank=True)
     program_description = models.CharField(max_length=100, null=True, blank=True)
     insta_link = models.CharField(max_length=100, null=True, blank=True)
--- backend/members/models.py
+++ backend/members/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Members(models.Model):
     year = models.IntegerField()#필수 데이터 
-    img = models.ImageField(upload_to='members/',blank=True) #이미지명은 학번 기준으로 하면 관리하기 좋을듯
+    img = models.ImageField(upload_to='static/members/',blank=True) #이미지명은 학번 기준으로 하면 관리하기 좋을듯
     std_id=models.IntegerField(unique=True)
     name = models.CharField(max_length=100,unique=False)
     comment = models.TextField(max_length=12, blank=True,unique=False)#공백 가능
